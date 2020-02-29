146ee388685aee627e3a35e4e533c55f1ae51933
Pavel Solikov <psolikov15@gmail.com>
2020-02-29 15:43:24 +0300

    added hw2

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -7,6 +7,7 @@ __all__ = [
 from typing import List, Optional, Tuple
 
 import numpy as np
+import cv2
 
 from corners import CornerStorage
 from data3d import CameraParameters, PointCloud, Pose
@@ -16,8 +17,49 @@ from _camtrack import (
     create_cli,
     calc_point_cloud_colors,
     to_opencv_camera_mat3x3,
-    view_mat3x4_to_pose
+    view_mat3x4_to_pose,
+    pose_to_view_mat3x4,
+    build_correspondences,
+    triangulate_correspondences,
+    TriangulationParameters,
+    rodrigues_and_translation_to_view_mat3x4
 )
+
+
+def _track_all(corner_storage, mats, real_points, intrinsic_mat, tr_params, min_good_points=10):
+    recalc = True
+    while recalc:
+        recalc = False
+        for i in range(len(corner_storage)):
+            frame_corners = corner_storage[i]
+            if mats[i] is None:
+                idx = frame_corners.ids.squeeze()
+                good_idx = np.array(list(map(lambda x: x is not None, real_points[idx])))
+                good_frame_points = frame_corners.points[good_idx]
+                if len(good_frame_points) >= min_good_points:
+                    retval, rvec, tvec, inliers = cv2.solvePnPRansac(
+                        np.stack(real_points[idx[good_idx]]),
+                        good_frame_points, intrinsic_mat,
+                        None)
+                    if retval:
+                        mats[i] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
+                        filter_outliers = np.setdiff1d(idx,
+                                                       np.intersect1d(inliers.squeeze(), idx))
+                        real_points[filter_outliers] = None
+                if mats[i] is not None:
+                    recalc = True
+                    for j in range(len(corner_storage)):
+                        if j != i and mats[j] is not None:
+                            corrs = build_correspondences(frame_corners,
+                                                          corner_storage[j])
+                            points, idx, _ = triangulate_correspondences(corrs, mats[i],
+                                                                         mats[j],
+                                                                         intrinsic_mat, tr_params)
+                            for id, point in zip(idx, points):
+                                if real_points[id] is None:
+                                    real_points[id] = point
+
+    return mats, real_points
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
@@ -37,6 +79,27 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
 
     # TODO: implement
     view_mats, point_cloud_builder = [], PointCloudBuilder()
+
+    mats = np.repeat(None, len(corner_storage))
+    mats[known_view_1[0]] = pose_to_view_mat3x4(known_view_1[1])
+    mats[known_view_2[0]] = pose_to_view_mat3x4(known_view_2[1])
+    real_points = np.repeat(None, corner_storage.max_corner_id() + 1)
+    corrs = build_correspondences(corner_storage[known_view_1[0]], corner_storage[known_view_2[0]])
+    tr_params = TriangulationParameters(max_reprojection_error=50,
+                                        min_triangulation_angle_deg=0.01,
+                                        min_depth=0.01)
+    points, idx, _ = triangulate_correspondences(corrs, mats[known_view_1[0]], mats[known_view_2[0]],
+                                                 intrinsic_mat, tr_params)
+    for id, point in zip(idx, points):
+        if real_points[id] is None:
+            real_points[id] = point
+
+    mats, real_points = _track_all(corner_storage, mats, real_points, intrinsic_mat, tr_params)
+
+    view_mats = mats
+    real_points = list(filter(lambda x: x is not None, real_points))
+    point_cloud_ids = np.arange(0, len(real_points))
+    point_cloud_builder = PointCloudBuilder(point_cloud_ids, np.array(real_points))
 
     calc_point_cloud_colors(
         point_cloud_builder,
--- camtrack/corners.py
+++ camtrack/corners.py
@@ -14,6 +14,7 @@ import click
 import cv2
 import numpy as np
 import pims
+from numpy.linalg import norm
 
 from _corners import FrameCorners, CornerStorage, StorageImpl
 from _corners import dump, load, draw, without_short_tracks, create_cli
@@ -34,6 +35,16 @@ class _CornerStorageBuilder:
         return StorageImpl(item[1] for item in sorted(self._corners.items()))
 
 
+def _process_first_frame(image_1, CV_GFTT_PARAMS, BSIZE):
+    first_corners = cv2.goodFeaturesToTrack(image_1, *CV_GFTT_PARAMS)
+    corners = FrameCorners(
+        np.array(list(range(len(first_corners)))),
+        first_corners,
+        np.array(np.repeat(BSIZE, len(first_corners)))
+    )
+    return len(first_corners), corners
+
+
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
     image_0 = frame_sequence[0]
@@ -43,8 +54,50 @@ def _build_impl(frame_sequence: pims.FramesSequence,
         np.array([55])
     )
     builder.set_corners_at_frame(0, corners)
+
+    FILTER_THRESH = 10
+
+    CV_GFTT_MAX_CORNERS = 500
+    CV_GFTT_QUALITY_LVL = 0.001
+    CV_GFTT_MIN_DIST = 10
+    CV_GFTT_CORNERS = None
+    CV_GFTT_MASK = None
+    CV_GFTT_BSIZE = 10
+    CV_GFTT_HARRIS = False
+    CV_GFTT_K = 0.05
+
+    CV_OFPLK_NEXT_POINTS = None
+
+    CV_GFTT_PARAMS = [CV_GFTT_MAX_CORNERS, CV_GFTT_QUALITY_LVL,
+                      CV_GFTT_MIN_DIST, CV_GFTT_CORNERS, CV_GFTT_MASK,
+                      CV_GFTT_BSIZE, CV_GFTT_HARRIS, CV_GFTT_K]
+    CV_OFPLK_KPARAMS = dict(winSize=(11, 11))
+
+    current_id, current_corners = _process_first_frame(image_0, CV_GFTT_PARAMS, CV_GFTT_BSIZE)
+    builder.set_corners_at_frame(0, current_corners)
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        cv2_corners = cv2.goodFeaturesToTrack(image_1, *CV_GFTT_PARAMS)
+        image_0_mono = np.round((image_0 * 255)).astype('uint8')
+        image_1_mono = np.round((image_1 * 255)).astype('uint8')
+        features, status, track_error = cv2.calcOpticalFlowPyrLK(image_0_mono, image_1_mono,
+                                                                 current_corners.points.reshape(-1, 1, 2).astype(
+                                                                     'float32'),
+                                                                 CV_OFPLK_NEXT_POINTS,
+                                                                 **CV_OFPLK_KPARAMS)
+        status = status.squeeze()
+        mask_good = (status == 1)
+        features, current_ids = features[mask_good], current_corners.ids[mask_good]
+        cv2_corners = np.array(list(filter(lambda x: np.min(norm(features - x, axis=-1)) > FILTER_THRESH, cv2_corners)))
+        cv2_corners_ids = np.arange(current_id, current_id + len(cv2_corners))
+        current_id = current_id + len(cv2_corners)
+        corners = FrameCorners(
+            np.concatenate([current_ids.squeeze(), cv2_corners_ids]),
+            np.concatenate([features.reshape(-1, 1, 2), cv2_corners.reshape(-1, 1, 2)]),
+            np.array(np.repeat(10, len(features) + len(cv2_corners)))
+        )
+
         builder.set_corners_at_frame(frame, corners)
+        current_corners = corners
         image_0 = image_1
 
 
